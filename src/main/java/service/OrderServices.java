69b377a20f8b18a44985050328d1fe622cc9a66e
ConwayQA <luke.conway@academytrainee.com>
2020-04-14 15:29:14 +0100

    Added OrderServices class

---
+++ src/main/java/service/OrderServices.java
@@ -0,0 +1,36 @@
+package service;
+
+import java.util.List;
+
+import persistance.dao.DAO;
+import persistance.domain.Order;
+
+public class OrderServices implements CrudService<Order>{
+	
+	private DAO<Order> orderDAO;
+	
+	public OrderServices(DAO<Order> orderDAO) {
+		this.orderDAO = orderDAO;
+	}
+
+	@Override
+	public List<Order> readAll() {
+		return orderDAO.readAll();
+	}
+
+	@Override
+	public Order create(Order createOrder) {
+		return orderDAO.create(createOrder);
+	}
+
+	@Override
+	public Order update(Order updateOrder) {
+		return orderDAO.update(updateOrder);
+	}
+
+	@Override
+	public void delete(Long id) {
+		orderDAO.delete(id);
+	}
+
+}
