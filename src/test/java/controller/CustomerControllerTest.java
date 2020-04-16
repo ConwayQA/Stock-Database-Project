7e1f15d53e4939227ab598b71ccd222b77302c94
ConwayQA <luke.conway@academytrainee.com>
2020-04-16 15:50:47 +0100

    Tester Hotfix

--- src/test/java/controller/CustomerControllerTest.java
+++ src/test/java/controller/CustomerControllerTest.java
@@ -6,39 +6,4 @@ import org.junit.Test;
 
 public class CustomerControllerTest {
 
-	@Test
-	public void testCustomerController() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testGetInput() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testReadAll() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testCreate() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testUpdate() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testDelete() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testInputCustomerData() {
-		fail("Not yet implemented"); // TODO
-	}
-
 }
